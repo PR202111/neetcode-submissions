bedad57fd159ca39724e7119ccb0df87a07055a3
Prashant Raj <prashant_b240272cs@nitc.ac.in>
2026-06-04 00:57:15 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==0) return 0;
+        unordered_map<int,int> map;
+        
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]] = 1;
+        }
+        int ans = 1;
+        for(int i=0;i<nums.size();i++){
+            int count = 1;
+            if(map.find(nums[i]-1) == map.end()){
+                int num = nums[i]+1;
+                while(map.find(num) != map.end()){
+                    num++;
+                    count++;
+                }
+
+                ans = max(ans,count);
+            }
+        }
+
+        return ans;
+    }
+};

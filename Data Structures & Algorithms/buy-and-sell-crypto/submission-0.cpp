ee92ec744dcc602584007c62489eda2e6f91007c
Prashant Raj <prashantraj251195@gmail.com>
2026-06-27 20:26:10 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int cp = prices[0];
+        for(int i=1;i<prices.size();i++){
+            profit = max(profit,prices[i]-cp);
+            if(cp > prices[i]){
+                cp = prices[i];
+            }
+        }
+
+        return profit;
+    }
+};

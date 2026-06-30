76656bf3591a0a4415daf036ca546fd2b2d7570f
Prashant Raj <prashantraj251195@gmail.com>
2026-06-30 21:12:11 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPossible(unordered_map<char,int>& map1,unordered_map<char,int> map2){
+
+        for(auto &p : map1){
+            if(map2[p.first] < p.second)
+                return false;
+        }
+        return true;
+        
+      
+    }
+    string minWindow(string s, string t) {
+        unordered_map<char,int> map1;
+        unordered_map<char,int> map2;
+        for(char c:t){
+            map1[c]++;
+        }
+        int l=0;
+        int ll = -1;
+        int rr = -1;
+        int len = INT_MAX;
+        for(int i=0;i<s.length();i++){
+            map2[s[i]]++;
+            while(l<=i && isPossible(map1,map2)){
+                map2[s[l]]--;
+                if(len > i-l+1){
+                    len = i-l+1;
+                    ll = l;
+                    rr = i;
+                }
+                l++;
+            }
+        }
+        if(ll == -1) return "";
+
+        string ans = s.substr(ll,rr-ll+1);
+
+        return ans;
+        
+    }
+};
